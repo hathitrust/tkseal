e260eb0326f6e0793a2e67e47ca4c2d4bf199047
K'Ron Spar <kspar@umich.edu>
2025-10-14 15:55:21 -0400

    Removed inconsistent testing usage in test_kubectl.py

--- tests/test_kubectl.py
+++ tests/test_kubectl.py
@@ -7,13 +7,11 @@ from tkseal.kubectl import KubeCtl
 
 
 class TestKubectl:
-    #@patch("tkseal.kubectl.shutil.which", return_value="/usr/local/bin/kubectl")
-
-    def test_kubectl_exists_returns_true_when_installed(self, mocker):
+    @patch("tkseal.kubectl.shutil.which", return_value="/usr/local/bin/kubectl")
+    def test_kubectl_exists_returns_true_when_installed(self, mock_which):
         """Return True when kubectl is on PATH.
         This test mocks shutil.which to simulate kubectl being installed.
         """
-        mock_which = mocker.patch("tkseal.kubectl.shutil.which", return_value="/usr/local/bin/kubectl")
         assert KubeCtl.exists() is True
         mock_which.assert_called_once_with("kubectl")
 
